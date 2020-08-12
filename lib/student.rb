8a492a60d3dd6c6f8e260f0b0168c8c75a13bcad
Adam Palmer <theadampalmer@gmail.com>
2020-08-12 23:15:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,14 @@ attr_accessor :id, :name, :grade
     end
     
     def self.find_by_name
-      
+        sql = <<-SQL
+        SELECT * FROM students
+        WHERE name = ?
+        LIMIT 1
+        SQL
+        DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+        end.first
     end
     
     def update
