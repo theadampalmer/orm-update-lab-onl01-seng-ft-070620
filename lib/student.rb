c54f417ff2687117f94f0e807e80426e1dab2b04
Adam Palmer <theadampalmer@gmail.com>
2020-08-12 23:17:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,16 @@ attr_accessor :id, :name, :grade
     end
     
     def save
-      
+        if self.id
+          self.update
+        else
+          sql = <<-SQL
+          INSERT INTO students (name, grade)
+          VALUES (?, ?);
+          SQL
+          DB[:conn].execute(sql, self.name, self.grade)
+          @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+        end
     end
     
     def self.create(name, grade)
