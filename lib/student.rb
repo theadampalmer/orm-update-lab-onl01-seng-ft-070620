530040dee029cb5d2bdb014763c2e0cbcb6392ea
Adam Palmer <theadampalmer@gmail.com>
2020-08-12 23:11:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,15 @@ attr_accessor :id, :name, :grade
     end
 
     def self.create_table
-      
+         sql = <<-SQL
+       CREATE TABLE IF NOT EXISTS students
+       (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+       )
+       SQL
+       DB[:conn].execute(sql)
     end
     
     def self.drop_table
