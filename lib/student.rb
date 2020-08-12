45a9d185ea3c43d3c51ad637b0a86e04c438fbb9
Adam Palmer <theadampalmer@gmail.com>
2020-08-12 23:14:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,8 +37,10 @@ attr_accessor :id, :name, :grade
         student.save
     end
     
-    def self.new_from_db
-      
+    def self.new_from_db(row)
+        student = self.new(row[1], row[2], row[0])
+        student.save
+        student
     end
     
     def self.find_by_name
